36afeb2edc3e5d4283afe70e4be02e4dc0160d28
Felix <felix.mars@outlook.com>
2024-05-08 17:45:05 +0200

    update requirements

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import cv2
 import logging
 import os
 import time
+import magic
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -15,6 +16,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+i = 0
 @app.route('/process_video', methods=['POST'])
 def process_video():
     log.debug('Processing video data: %s', request.files)
@@ -25,24 +27,39 @@ def process_video():
         return 'No video file in request', 400
     
     try:
-        time.sleep(1)
         video_file = request.files['video']
         log.debug('Video file: %s', video_file)
 
         # Save the video file to disk
+        global i
+        i = i + 1
+        # video_path = str(i) + 'webcam_stream.webm'
         video_path = 'webcam_stream.webm'
+        
+
         video_file.save(video_path)
+
+        # Check the file size
+        file_size = os.path.getsize(video_path)
+        if file_size == 0:
+            log.error('Empty video file')
+            return 'Empty video file', 400
+
+        # # Check the file format
+        # mime_type = magic.from_file(video_path, mime=True)
+        # if mime_type != 'video/webm':
+        #     log.error('Invalid file format: %s', mime_type)
+        #     return 'Invalid file format', 400
 
         # Open the video file
         cap = cv2.VideoCapture(video_path)
         ret, frame = cap.read()
 
         if ret:
-            cv2.imwrite('webcam_frame.jpg', frame)
+            cv2.imwrite(str(i) + 'webcam_frame.jpg', frame)
             cap.release()
 
         log.info('Video data processed successfully')
-        exit()
         return 'Video data processed successfully', 200
     except Exception as e:
         log.error(f'Error processing video data: {e}')
