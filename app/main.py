159fc66cf6dffd7b3fe7459d922409fd53ecbe9c
Aaron Philipp Kreis <ukxbv@student.kit.edu>
2024-06-21 12:36:30 +0000

    Update file main.py

--- app/main.py
+++ app/main.py
@@ -32,41 +32,64 @@ def get_stock():
         return "No stock available", 404
     return stock, 200
 
+@app.route("add_product_by_photo" , methods=["POST"])
+def add_product_by_photo():
+   #get products from master data
+   masterdata = api.get("objects/products")
+   generated_name = 'PetersErbsen'
+   for product in masterdata:
+       if product['name'] == generated_name:
+           product_id = product['id']
+           add_product(product_id)
+           #print(product_id)
+           break
+   else:
+       response, status = add_product_to_md(generated_name)
+       created_product_id = response['created_object_id']
+       #print(created_product_id)
+       add_product(created_product_id)
+
 @app.route("add_product_to_md", methods=["POST"])
-def add_product_to_md():
-    with app.app_context():
-        name = "New product xy"
-        description = "asfsfsfsf"
-        location_id = 1
-        qu_id_purchase = 1
-        qu_id_stock = 1
-        
+def add_product_to_md(name):
+        name #= "New product xyx"
+        description = ""
+        location_id = 2
+        qu_id_purchase = 2 #pack=3 oder Piece=2
+        qu_id_stock = 2 #pack=3 oder Piece=2 
+        #qu_factor_purchase_to_stock = 1
+
         data = {
             "name": name,
             "description": description,
             "location_id": location_id,
             "qu_id_purchase": qu_id_purchase,
             "qu_id_stock": qu_id_stock,
+            #"qu_factor_purchase_to_stock": qu_factor_purchase_to_stock
         }
 
     response = api.post(f'objects/products', data)
-    return response.json, 200
+    #print(response.json())
+    return response.json(), 200
 
 @app.route("/add_product", methods=["POST"])
-def add_product():
-    with app.app_context():
-        product_id = request.form.get('product_id')
+def add_product(product_id):
+        product_id  # = 8
         amount = 1
         price = 1
-        best_before_date = request.form.get('best_before_date')
+        transaction_type = "purchase"
+        best_before_date = "2025-01-01"
+        
 
         data = {
             "amount": amount,
             "best_before_date": best_before_date,
-            "price": price
+            "transaction_type": transaction_type,
+            "price": price,
+            #'note': "Aaron",
         }
 
         response = api.post(f'stock/products/{product_id}/add', data)
+        #print(response.status_code, response.text)
         return response.json, 200
 
 @app.route("/remove_product", methods=["POST"])
